490ea0c60cf3c2692899820dac56d0e2a421258c
bakytkhamraev <khamraevbakyt@gmail.com>
2019-10-19 22:04:18 +0600

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	public void printInfo(XXXXXX obj);
+        Player player = new Player();
+        player.setAge(22);
+        player.setForce(50);
+        player.setGender("male");
+        printInfo(player);
+
+
+
+        Basketballplayer basketballplayer = new Basketballplayer();
+        basketballplayer.setAge(55);
+        basketballplayer.setForce(20);
+        basketballplayer.setGender("famile");
+        printInfo(basketballplayer);
+
+
+
+
+        Sportsman [] players = {new Player(), new Basketballplayer()};
+        for (int i = 0; i <players.length ; i++) {
+            players[i].doExercises();
+            players[i].strikeForce();
+            players[i].sayYourname();
+
+        }
     }
+    public static void printInfo(Sportsman a){
+        System.out.println(a.getAge()+ " лет "+a.getForce()+" ньютона "+a.getGender());
+
+    }
+
 }
+
